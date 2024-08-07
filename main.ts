261689052dd081fd75d078da4c1ae2e42689137a
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2024-08-07 10:59:47 -0600

    feat(main): Added validation to run command when main.ts is the main module

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { Command } from "@cliffy/command";
 import { DESCRIPTION, NAME } from "@/util/constant.ts";
+import { colors } from "@cliffy/ansi/colors";
 
 export async function version() {
   const { default: configs } = await import("./deno.json", {
@@ -7,11 +8,20 @@ export async function version() {
       type: "json",
     },
   });
-  console.log(`${NAME} v${configs.version}`);
+
+  const info = colors.bold.yellow;
+  console.log(info(`${NAME} v${configs.version}`));
 }
 
-await new Command()
+const command = new Command()
   .name(NAME)
   .description(DESCRIPTION)
   .versionOption("-v, --version", "Print version info.", version)
-  .parse(Deno.args);
+  .option("-e, --example", "Example")
+  .action((options, ...args) => {
+    console.log(options, args);
+  });
+
+if (import.meta.main) {
+  await command.parse(Deno.args);
+}
